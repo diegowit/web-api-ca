c453f5127c18894ce74a5cb7ad95d8e90c91f231
Diego <20098757@mail.wit.ie>
2025-12-23 08:11:26 +0000

    Login and singup added to  tmdb.api

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -11,3 +11,21 @@ export const getMovies = async () => {
 
     return await response.json();
 };
+
+
+export const login = async (username, password) => {
+  const response = await fetch("http://localhost:8080/api/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+};
+export const signup = async (username, password) => {
+  const response = await fetch("http://localhost:8080/api/users?action=register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+};
